3d8fb6382ca8c6323209e7b7fd1d789698aabbea
saimon494 <saimon.494@gmail.com>
2017-08-17 21:41:28 +0300

    chapter 5 task 12

--- src/chapter5/Main.java
+++ src/chapter5/Main.java
@@ -1,8 +1,106 @@
 package chapter5;
 
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+//        Book novel = new Book(true);  // 11
+//        novel.checkIn();
+//        new Book(true);
+//        System.gc();
+//        novel.finalize();
+//        F f = new F();    //10
+//        f.finalize();
+//        Test t = new Test();  //7
+//        System.out.println(t);
+//        Dog dog = new Dog();  // 5
+//        dog.bark("bark",4);
+//        dog.bark(3, "yelp");
     }
 }
+
+// 11, 12 - finalize()
+//class Book {
+//    boolean checkedOut = false;
+//
+//    Book(boolean checkOut) {
+//        checkedOut = checkOut;
+//    }
+//
+//    void checkIn() {
+//        checkedOut = false;
+//    }
+//
+//    public void finalize() {
+//        if(checkedOut) {
+//            System.out.println("Error: checkedOut");
+//        }
+//    }
+//}
+
+// 10 - finalize()
+//class F {
+//    public void finalize(){
+//        System.out.println("finalize");
+//    }
+//}
+
+// 9 - calling constructor from another
+//class Test {
+//    Test(int i){
+//        this(3, 4);
+//    }
+//
+//    Test(int i, int j){
+//
+//    }
+//}
+
+// 8 - this, use in non-static class
+//class Test {
+//    void m1(){
+//        m2();
+//        this.m2(); // wrong
+//    }
+//
+//    void m2(){
+//
+//    }
+//}
+
+// 7
+//class Test {
+//}
+
+// 5, 6 - overloading
+//class Dog{
+//    void bark(String s, int times) {
+//        System.out.println("Dog " + s + " " + times + " times");
+//    }
+//    void bark(int times, String s) {
+//        System.out.println(times + " times Dog " + s);
+//    }
+//}
+
+// 3, 4
+//class Test {
+//    Test() {
+//        System.out.println("Default constructor");
+//    }
+//
+//    Test(String s) {
+//        System.out.println(s);
+//    }
+//}
+
+// 1, 2
+//class Test {
+//    String field = " ";
+//
+//    public Test(String newField) {
+//        newField = " ";
+//    }
+//}
+
